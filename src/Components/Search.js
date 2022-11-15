0bbb3b6a66eb060b13f66f5d94332575273a7940
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-15 12:48:25 -0500

    Created search bar

---
+++ src/Components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search() {
+    return (
+        <div>
+            <input type='text' placeholder='Enter search'/>
+            <input type='submit' />
+        </div>
+    );
+}
+
+export default Search;
