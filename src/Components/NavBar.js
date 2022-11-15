bc862ab6e45e47b722452a63eddcfc5a59058579
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-15 12:53:09 -0500

    Added Nav bar and search bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,10 @@ import React from 'react';
 function NavBar() {
     return (
         <div className='navBar'>
-            
+            <nav>
+                <h1>About</h1>
+                <h2>Home</h2>
+            </nav>
         </div>
     );
 }
