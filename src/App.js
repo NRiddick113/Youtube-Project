84b5a82ed5563f9a73886d4077f9c695c5dc36e9
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-16 10:57:03 -0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home';
+import About from './Components/About'
+import Video from './Components/Video'
+import NavBar from './Components/NavBar';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/videos/:id" element={<Video />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import NavBar from './NavBar';
-import Search from './Search';
+
 
 function Home() {
     return (
         <div>
-            <NavBar />
-            <Search />
+            <input type='text' placeholder='Enter search'/>
+            <input type='submit' />
+
+            <p>No Search Results Yet!, Please submit a search above!</p>
         </div>
     );
 }
