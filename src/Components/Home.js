3c3953a7a4207d17791896236b4ff44f18ccc50b
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-16 12:56:09 -0500

    Functioning search bar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import { useState } from 'react';
+import Video from './Video';
 import './Home.css'
 
 
 function Home() {
+
+    const [text, setText] = useState("")
+    // const [search, setSearch] = useState("")
+    const [videos, setVideos] = useState([])
+
+    const handleTextChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        // setSearch(text)
+
+        if (text === ""){
+            setVideos([])
+        } else { 
+            fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${text}&key=${process.env.REACT_APP_API_KEY}&part=snippet`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                setVideos(res.items)
+            })
+            .catch(err => console.log(err))
+        }
+        console.log(videos)
+    }
+
     return (
         <div>
-            <input type='text' placeholder='Enter search'/>
-            <input type='submit' />
-
-            <p>No Search Results Yet! Please submit a search above!</p>
+            <input type='text' placeholder='Enter search' onChange={handleTextChange} />
+            <input type='submit' onClick={handleSubmit} />
+            {
+                //Error message if nothing searched
+                videos.length === 0 && <p>No Search Results Yet! Please submit a search above!</p> 
+            }
+            {
+                videos.map((video) => {
+                    return (
+                    <p>{video.snippet.title}</p>
+                    )
+                })
+            }
         </div>
     );
 }
---
+++ src/Components/Video.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Video({name}) {
+    return (
+        <div>
+            <p>{name}</p>
+        </div>
+    );
+}
+
+export default Video;
