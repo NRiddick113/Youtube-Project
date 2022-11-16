d8ba6bad9da67f7dbabc421a58746979ed0d786f
NRiddick113 <NatashaRiddick@pursuit.org>
2022-11-16 11:36:00 -0500

    started about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function About() {
     return (
-        <div>
-            
+        <div className='about' >
+            <p>A brief project description.</p>
+
+            <p>Kylie</p>
+            <p>Hi, my name is Natasha! I'm a Full Stack Software Engineer with experience in Node.js, React, HTML5, CSS, Jest, Git, npm and JavaScript. I am currently enrolled in Pursuit to learn and gain more skill in being a Full Stack Software Engineer. I created website in the Front End using HTML5 and React. My expertise lies within front-end web apps, and the main languages in my tech stack are JavaScript, React, and of course HTML/CSS.I love to listen to music, hang out with family, and find new escape room/ puzzles in New York City.
+</p>
         </div>
     );
 }
