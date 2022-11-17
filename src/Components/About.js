e009d05c123f0552a10b2c290f8e0bf0d932240f
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-17 13:47:27 -0500

    Added bio to About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './About.css'
 
 function About() {
     return (
         <div className='about' >
-            <p>A brief project description.</p>
+            <p>This YouTube Clone project uses the YouTube API data to mimic YouTube's functionality.  On this application, you will be able to search videos by keyword, view selected videos and leave comments.  Happy viewing!</p>
 
-            <p>Kylie</p>
-            <p>Hi, my name is Natasha! I'm a Full Stack Software Engineer with experience in Node.js, React, HTML5, CSS, Jest, Git, npm and JavaScript. I am currently enrolled in Pursuit to learn and gain more skill in being a Full Stack Software Engineer. I created website in the Front End using HTML5 and React. My expertise lies within front-end web apps, and the main languages in my tech stack are JavaScript, React, and of course HTML/CSS.I love to listen to music, hang out with family, and find new escape room/ puzzles in New York City.
-</p>
+            <p>Hi, my name is Kylie! I am a Pursuit fellow building upon skills to become a full stack web developer. Outside of technology, I am also passionate about animals and healthcare, which has led me on a path within the veterinary field. I also love travelling and going to music festivals. My love for music and its ability to unite groups of people makes me appreciate applications such as YouTube for making it so widely accessible. </p>
+            <p>Please follow my other work on <a href='https://github.com/Kylie-Lang1'>GitHub</a></p>
+            <p>Hi, my name is Natasha! I'm a Full Stack Software Engineer with experience in Node.js, React, HTML5, CSS, Jest, Git, npm and JavaScript. I am currently enrolled in Pursuit to learn and gain more skill in being a Full Stack Software Engineer. I created website in the Front End using HTML5 and React. My expertise lies within front-end web apps, and the main languages in my tech stack are JavaScript, React, and of course HTML/CSS.I love to listen to music, hang out with family, and find new escape room/ puzzles in New York City.</p>
         </div>
     );
 }
