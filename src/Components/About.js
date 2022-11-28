1d7b770e28a507d50776e1399d3a6496c1982b46
NRiddick113 <NatashaRiddick@pursuit.org>
2022-11-28 11:03:41 -0500

    css img

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,10 @@ function About() {
 
             <p>Hi, my name is Kylie! I am a Pursuit fellow building upon skills to become a full stack web developer. Outside of technology, I am also passionate about animals and healthcare, which has led me on a path within the veterinary field. I also love travelling and going to music festivals. My love for music and its ability to unite groups of people makes me appreciate applications such as YouTube for making it so widely accessible. </p>
             <p>Please follow my other work on <a href='https://github.com/Kylie-Lang1'>GitHub</a></p>
+
+            <img src=''></img>
             <p>Hi, my name is Natasha! I'm a Full Stack Software Engineer with experience in Node.js, React, HTML5, CSS, Jest, Git, npm and JavaScript. I am currently enrolled in Pursuit to learn and gain more skill in being a Full Stack Software Engineer. I created website in the Front End using HTML5 and React. My expertise lies within front-end web apps, and the main languages in my tech stack are JavaScript, React, and of course HTML/CSS.I love to listen to music, hang out with family, and find new escape room/ puzzles in New York City.</p>
+            <p>Please follow my other work on <a href='https://github.com/Nriddick113'>GitHub</a>. Also you can connected with me on <a href='https://www.linkedin.com/in/natasha-riddick/'>Linkedin</a></p>
         </div>
     );
 }
